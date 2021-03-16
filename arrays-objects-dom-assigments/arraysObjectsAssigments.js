7214c22d6caf5b7910a3111b32f54413cb747147
vwede oteghele <louisshaw75@gmail.com>
2021-03-15 22:32:55 -0700

    assigment to create 3 obects,3 arrays and object array

---
+++ arrays-objects-dom-assigments/arraysObjectsAssigments.js
@@ -0,0 +1,88 @@
+ //javascript objects with key value printGreatestNumber
+
+ const Table = {
+   type : "wooden table",
+   texture : "rough",
+   numbers of legs: 4,
+   strength : true,
+   manufacturers : { 
+      name : "ikea",
+      location : "USA",
+      business : "retail chain",
+      pre-assembly : false,
+      home-delivery : false,
+      }
+   wood : "timber",
+   popularity : "high",
+   warranty : "25 years",
+   storage : "none",
+   finishings : "shiny"
+ }
+
+
+ const Music = {
+   entertainment : true,
+   genres : {
+     name : "rock",
+     band : "beattles",
+     sub-genre : "pop rock",
+     artistes : "jimi hendrix",
+     fashion : "tough"
+   }
+   sound : true,
+   structure : "rymthm",
+   duration : "5 minutes",
+   music-concert : "cochella",
+   music-awards : "grammy's",
+   music-school : "british school of art",
+   streaming : "spotify",
+   industry : "hospitality",
+   revenue : "#100 billion",
+ }
+
+
+const ElonMusk = {
+  birthday : "28 june",
+  industry : "technology",
+  Ex-wife : ["talulah riley","justine musk"],
+    Girfriend : {
+      name :"Grimes",
+      nationality : "canadian",
+      education : "mcgill university",
+      genre : "mixed",
+      white : true,
+    }
+  Nationality : "South African",
+  profession : "business man",
+  height : "1.88m",
+  religion : "agnostic",
+  companies : 5,
+  salary : "$1",
+}
+
+
+//arrays with 3 sub arrays
+
+
+const designers = [["balenciaga","gucci","louis vuitton"],["Demna Gvasalia","alexander michele","virgil abloh"],["guccio gucci","christobal balenciaga","louis vuitton"]]
+
+
+
+const food = [["spaghetti","rice","chicken soup"],["orange","watermelon","kiwi"],["vanilla","chocolate","pudding"]]
+
+
+const hobbies = [["golf","polo","formula one"],["chess","scrabble","word puzzle"],["football","tennis","basketball"]]
+
+
+const tech = [
+  {
+    name : "fintech",
+    industry : "banking",
+    companies : ["paystack","flutter","carbon"]
+  }
+  {
+    name : "E-commerce",
+    industry : "retail",
+    companies : ["jumia","amazon","ebay"]
+  }
+]
